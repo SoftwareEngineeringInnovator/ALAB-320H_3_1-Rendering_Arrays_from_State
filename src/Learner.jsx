7c9bb7f3359bbaa75f7868a4479bebd7a4d696f8
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-20 15:16:19 -0400

    create Score component and render learner scores

--- src/Learner.jsx
+++ src/Learner.jsx
@@ -1,9 +1,24 @@
-// Learner function to display Learner name and bio
+// Import the Score from each learner
+import Score from './Score'
+
+// Learner function
 export default function Learner({ learner }) {
   return (
     <section>
+      {/* Display the learner's name and biography */}
       <h2>{learner.name}</h2>
       <p>{learner.bio}</p>
+
+      <h3>Scores</h3>
+
+      {/* We go through the learner's scores array and create one Score
+          component for every score object */}
+      {learner.scores.map((score) => (
+        <Score
+          key={score.date}
+          score={score}
+        />
+      ))}
     </section>
   )
 }
